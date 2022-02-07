f1df1f5e741e057bdc8d02819a57792b93308175
Kelvin Crepaldi <kelvin.creps@hotmail.com>
2022-02-06 22:34:28 -0300

    corrigido

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -4,7 +4,6 @@ from app.routes.series_route import bp as bp_register_route
 
 bp_api = Blueprint("api", __name__, url_prefix="/series")
 
-
 def init_app(app: Flask):
     
     bp_api.register_blueprint(bp_register_route)
